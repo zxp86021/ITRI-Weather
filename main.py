5bbad58f9d99ac32926c04ca67254d4f8a309073
richegg <zxp86021@gmail.com>
2018-02-10 15:14:41 +0800

    數值都收到了

--- main.py
+++ main.py
@@ -4,11 +4,42 @@ import time
 import serial
 import json
 import binascii
-
+import pigpio
+import RPi.GPIO as GPIO
+import Adafruit_WS2801
+import Adafruit_GPIO.SPI as SPI
 import Adafruit_DHT
+
+"""
+GPIO 17 PM2.5 strip Red
+GPIO 22 PM2.5 strip Blue
+GPIO 27 PM2.5 strip Green
+GPIO 5 PM2.5 Level1
+GPIO 6 PM2,5 Level2
+GPIO 13 PM2.5 Level3
+GPIO 19 PM2.5 Level4
+GPIO 26 PM2.5 Level5
+GPIO 12 PM2.5 Level6
+
+GPIO 11 UV_CLOCK
+GPIO 10 UV_DOUT
+GPIO 21 temperature_CLOCK
+GPIO 20 temperature_DOUT
+"""
 
 humidity, temperature = Adafruit_DHT.read_retry(22, 4)
 arduino = serial.Serial('/dev/ttyACM0', 9600)
+
+# Configure the count of pixels:
+PIXEL_COUNT = 32
+
+UV_CLOCK = 11
+UV_DOUT = 10
+temperature_CLOCK = 21
+temperature_DOUT = 20
+
+UV_pixels = Adafruit_WS2801.WS2801Pixels(PIXEL_COUNT, clk=UV_CLOCK, do=UV_DOUT)
+temperature_pixels = Adafruit_WS2801.WS2801Pixels(PIXEL_COUNT, clk=temperature_CLOCK, do=temperature_DOUT)
 
 class pmsA003():
     def __init__(self, dev):
@@ -61,6 +92,22 @@ class pmsA003():
         return d
 
 
+pi_led = pigpio.pi()
+pi_led.set_PWM_dutycycle(17, 0)
+pi_led.set_PWM_dutycycle(22, 0)
+pi_led.set_PWM_dutycycle(27, 0)
+pi_led.write(5, 0)
+pi_led.write(6, 0)
+pi_led.write(13, 0)
+pi_led.write(19, 0)
+pi_led.write(26, 0)
+pi_led.write(12, 0)
+# Clear ws2801 all the pixels to turn them off.
+UV_pixels.clear()
+UV_pixels.show()  # Make sure to call show() after changing any pixels!
+temperature_pixels.clear()
+temperature_pixels.show()  # Make sure to call show() after changing any pixels!
+
 # Un-comment the line below to convert the temperature to Fahrenheit.
 # temperature = temperature * 9/5.0 + 32
 
@@ -70,19 +117,98 @@ class pmsA003():
 # If this happens try again!
 while True:
     if humidity is not None and temperature is not None:
-        print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
+        if temperature <= 10:
+            print('<10')
+        elif 10 < temperature <= 20:
+            print('11-20')
+        elif 20 < temperature <= 27:
+            print('21-27')
+        elif 27 < temperature <= 30:
+            print('28-30')
+        else:
+            print('30+')
+        #print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
     else:
         print('Failed to get DHT reading')
 
     try:
         arduino_json_raw = arduino.readline()
         arduino_json = json.loads(arduino_json_raw)
-        print(arduino_json_raw)
+        # 4.49292074872709e-05x^2+0.0373750265x-0.3676207898
+        # 4.49292074872709e-05x^2+0.0373750265x-0.3676207898
+        uvIndex = (-1.706043956 * (arduino_json['UV'] ** 2)) + (118.3294205704 * arduino_json['UV']) + 59.3763736264
+        print(uvIndex)
+        print(arduino_json['UV'])
     except:
         print('Failed to get arduino reading')
 
     con = pmsA003('/dev/ttyUSB0')
     d = con.read_data()
-    print(d)
+    if d['apm25'] <= 50:
+        #print('0-50')
+        pi_led.set_PWM_dutycycle(17, 0)
+        pi_led.set_PWM_dutycycle(22, 0)
+        pi_led.set_PWM_dutycycle(27, 255)
+        pi_led.write(5, 1)
+        pi_led.write(6, 0)
+        pi_led.write(13, 0)
+        pi_led.write(19, 0)
+        pi_led.write(26, 0)
+        pi_led.write(12, 0)
+    elif 50 < d['apm25'] <= 100:
+        #print('51-100')
+        pi_led.set_PWM_dutycycle(17, 255)
+        pi_led.set_PWM_dutycycle(22, 0)
+        pi_led.set_PWM_dutycycle(27, 128)
+        pi_led.write(5, 1)
+        pi_led.write(6, 1)
+        pi_led.write(13, 0)
+        pi_led.write(19, 0)
+        pi_led.write(26, 0)
+        pi_led.write(12, 0)
+    elif 100 < d['apm25'] <= 150:
+        #print('101-150')
+        pi_led.set_PWM_dutycycle(17, 255)
+        pi_led.set_PWM_dutycycle(22, 0)
+        pi_led.set_PWM_dutycycle(27, 50)
+        pi_led.write(5, 1)
+        pi_led.write(6, 1)
+        pi_led.write(13, 1)
+        pi_led.write(19, 0)
+        pi_led.write(26, 0)
+        pi_led.write(12, 0)
+    elif 150 < d['apm25'] <= 200:
+        #print('151-200')
+        pi_led.set_PWM_dutycycle(17, 255)
+        pi_led.set_PWM_dutycycle(22, 0)
+        pi_led.set_PWM_dutycycle(27, 0)
+        pi_led.write(5, 1)
+        pi_led.write(6, 1)
+        pi_led.write(13, 1)
+        pi_led.write(19, 1)
+        pi_led.write(26, 0)
+        pi_led.write(12, 0)
+    elif 200 < d['apm25'] <= 300:
+        #print('201-300')
+        pi_led.set_PWM_dutycycle(17, 160)
+        pi_led.set_PWM_dutycycle(22, 240)
+        pi_led.set_PWM_dutycycle(27, 32)
+        pi_led.write(5, 1)
+        pi_led.write(6, 1)
+        pi_led.write(13, 1)
+        pi_led.write(19, 1)
+        pi_led.write(26, 1)
+        pi_led.write(12, 0)
+    else:
+        #print('>300')
+        pi_led.set_PWM_dutycycle(17, 139)
+        pi_led.set_PWM_dutycycle(22, 19)
+        pi_led.set_PWM_dutycycle(27, 69)
+        pi_led.write(5, 1)
+        pi_led.write(6, 1)
+        pi_led.write(13, 1)
+        pi_led.write(19, 1)
+        pi_led.write(26, 1)
+        pi_led.write(12, 1)
 
     time.sleep(2)
