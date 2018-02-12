4fa7d8956d3c439eb722ab13333f38e4aac0dc3f
richegg <zxp86021@gmail.com>
2018-02-12 13:52:44 +0800

    update DHT

--- main.py
+++ main.py
@@ -123,7 +123,7 @@ led_init()
 # If this happens try again!
 while True:
     try:
-        humidity, temperature = Adafruit_DHT.read_retry(22, 4)
+        humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, 4)
 
         if humidity is not None and temperature is not None:
             print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
